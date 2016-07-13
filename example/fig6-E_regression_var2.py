9ac737179b72c5e4d801af45829af478d94008f9
eifu <etomita18@gmail.com>
2016-07-13 13:14:19 -0400

    made it for all k minus versions

--- example/fig6-E_regression_var2.py
+++ example/fig6-E_regression_var2.py
@@ -22,21 +22,25 @@ TIME2 = 504  # 3 week in hour
 #     for i,km in enumerate(km_list):
 #         submain(round(km,4))
 #         print(i)
-
+#
+# def submain(k_minus):
+#     example_n = 500
 def main():
-    example_n = 500
     k_minus = 0.11
 
+    example_n = 500
     filename = "__k-{}/dumpdata3d__k-{}__{}examples.csv".format(k_minus, k_minus, example_n)
     read_data = io.read_dump3d_kp_time_hst(filename, TIME2)
 
     plt.style.use('ggplot')
     fig = plt.figure()
-    # ax = fig.add_subplot(1,1,1)
 
     hours = np.arange(8 * 24)
     print(read_data.shape)
     container = np.zeros((24, 8 * 24))
+
+    result = np.zeros((24,3),dtype=float)  # 24 versions of k+ with amax, gamma, betta
+
     for i, oneversion in enumerate(read_data):
 
         enrichment = np.zeros(8 * 24)  # enrichment is the timeseries of enrichment of locus of histone methylation
@@ -54,19 +58,23 @@ def main():
         ax = fig.add_subplot(4, 6, i + 1)
 
         ax.plot(hours, container[i], "-", color=colors[i])
+        a0 = max(enrichment) + 0.0001
         amax = max(enrichment) + 0.0001
 
         rev_enrichment = [np.log((amax - e) / amax) for e in enrichment]
-        ran = list(range(100))
-        gamma, beta, cmax, _, _ = stats.linregress(ran, rev_enrichment[:100])
-        ax.plot(hours, rev_enrichment, "--", color=colors[i])
+        ran = list(range(50))
+        gamma, beta, cmax, _, _ = stats.linregress(ran, rev_enrichment[:50])
+        ax.plot(hours, rev_enrichment, ".-", color=colors[i])
+        func = lambda x: gamma * x + beta
+        estimated_y = [func(x) for x in hours]
+        ax.plot(hours, estimated_y, '--', color='gray')
 
 
-        en100 = enrichment[:100]  # experimentally set up 50 first examples
+        en50 = enrichment[:50]  # experimentally set up 50 first examples
 
         for a in np.arange(amax, amax * 4, 0.001):
 
-            z = np.array([np.log((a - en) / a) for i, en in enumerate(en100)])
+            z = np.array([np.log((a - en) / a) for i, en in enumerate(en50)])
 
             slope, intercept, r_value, _, _ = stats.linregress(ran, z)
             if r_value > cmax:
@@ -80,28 +88,38 @@ def main():
         ax.set_yticklabels([0], fontsize=4)
 
         final_z = np.array([np.log((amax - en) / amax) for i, en in enumerate(enrichment)])
-        ax.plot(hours, final_z, "-", color=colors[23 - i])
+        ax.plot(hours, final_z, "-", color=colors[-i])
 
         func = lambda x: gamma * x + beta
         estimated_y = [func(x) for x in hours]
         ax.plot(hours, estimated_y, '--', color='black')
-        print("cmax {}   amax {}   gamma {}".format(cmax, amax, gamma))
+        print("cmax {}  a0 {} amax {}   gamma {}  beta {}".format(cmax,a0, amax, gamma, beta))
 
         ax.set_title(
             "cmax {} amax {}\n gamma {} beta{}".format(round(cmax, 4), round(amax, 4), round(gamma, 4), round(beta, 4)),
             fontsize=4)
 
+        result[i][0] = amax
+        result[i][1] = gamma
+        result[i][2] = beta
+
     # figure.figure6c_and_6e(fig, read_data, example_n)
 
     # plt.show()
 
-    # for i, en in enumerate(container):
-    #     print(en, max(en))
 
-    title = "fig6-CE/fig__testes5_regress__{}examples__k-{}.pdf".format(example_n, k_minus)
+    title = "fig6-CE/fig___regress__{}examples__k-{}.pdf".format(example_n, k_minus)
     pp = PdfPages(title)
     pp.savefig(fig)
     pp.close()
+
+
+    with open("fig6___{}example__k-{}.csv".format(example_n,k_minus), 'wb') as f:
+        np.savetxt(f,
+                   result,
+                   fmt='%f',
+                   delimiter=',',
+                   newline='\n')
 
 
 if __name__ == "__main__":
