ac1937c9b46091547c5e6f154f4f84226fa56215
eifu <etomita18@gmail.com>
2016-08-04 15:57:21 -0400

    change data collecting script to get knuc0->knuc1 data. enrichment over knuc change .py is a file that create a 3d figure of how histones forget their memory.

---
+++ example/enrichment_over_knuc_change.py
@@ -0,0 +1,66 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+
+dir1 = "example/data/withNUC/"
+example_n = 500
+
+
+def main():
+
+    kp = 0.145
+    km = 0.145
+
+    kn1 = 1
+    ka1 = 0
+
+    kn2 = 0
+    ka2 = 0
+
+    # read data
+    print("read data...")
+    filename = dir1 + "data{}/kn{}ka{}_kn{}ka{}/dumpdata2d__k+{}k-{}_{}examples.csv".format(example_n,
+                                                                                        round(kn1, 4),
+                                                                                        round(ka1, 4),
+                                                                                        round(kn2, 4),
+                                                                                        round(ka2, 4),
+                                                                                        round(kp, 4),
+                                                                                        round(km, 4),
+                                                                                        example_n)
+    hist = np.genfromtxt(filename, skip_header=0, skip_footer=0, delimiter=',')
+
+    # create a graph
+    print("create graph...")
+    plt.style.use('ggplot')
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1, projection="3d")
+
+    x = np.arange(81)
+    y = np.arange(24 * 7 * 3)
+
+    elements = (len(x)) * (len(y))
+    xpos, ypos = np.meshgrid(x, y)
+
+    xpos = xpos.flatten()
+    ypos = ypos.flatten()
+    zpos = np.zeros(elements)
+    dx = 0.5 * np.ones_like(zpos)
+    dy = dx.copy()
+    dz_acc = hist.flatten()
+
+    ax.bar3d(xpos, ypos, zpos, dx, dy, dz_acc, color='b')
+    ax.set_xlabel("histone pos")
+    ax.set_xticks((0, 35, 40, 45, 80))
+    ax.set_xticklabels((-40, -5, 0, 5, 40))
+    ax.set_ylabel("time")
+
+    ax.text2D(0.05, 0.95, "km:{}, kp:{}".format(km, kp), transform=ax.transAxes)
+
+    plt.show()
+    # save a graph
+    # print("save graph...")
+
+
+if __name__ == "__main__":
+    main()
--- example/fig6-E_data_collecting.py
+++ example/fig6-E_data_collecting.py
@@ -12,36 +12,35 @@ TIME2 = 504  # 3 week in hour
 
 HST_N = 81
 
-example_n = 300
-dir = 'example/data{}_withNUC/'.format(example_n)
+example_n = 500
+dir = 'example/data/withNUC/data{}/'.format(example_n)
 if not os.path.exists(dir):
     os.mkdir(dir)
 
 
 def main():
-    kn_list = np.arange(0.05, 0.96, 0.05)
     ka1 = 0
     ka2 = 0
-    kn1 = 0
+    kn1 = 1
+    kn2 = 0
 
     kmkppair = [(0.145, 0.145)]
 
-    for kn2 in kn_list:
-        dir2 = dir + "kn{}ka{}_kn{}ka{}/".format(round(kn1, 4), round(ka1, 4),round(kn2, 4), round(ka2, 4))
-        if not os.path.exists(dir2):
-            os.mkdir(dir2)
+    dir2 = dir + "kn{}ka{}_kn{}ka{}/".format(round(kn1, 4), round(ka1, 4), round(kn2, 4), round(ka2, 4))
+    if not os.path.exists(dir2):
+        os.mkdir(dir2)
 
-        for km, kp in kmkppair:
-            onekp_samplelist_genets = submain(kp, km, kn1, ka1,kn2,ka2)
-            print("kn:{}, ka:{} ,done km:{}, kp:{}".format(round(kn2, 4), round(ka2, 4), round(km, 4), round(kp, 4)))
+    for km, kp in kmkppair:
+        onekp_samplelist_genets = submain(kp, km, kn1, ka1, kn2, ka2)
+        print("kn:{}, ka:{} ,done km:{}, kp:{}".format(round(kn2, 4), round(ka2, 4), round(km, 4), round(kp, 4)))
 
-            compressed = io.compress_onekp_samplelist_hstseqts(onekp_samplelist_genets)
+        compressed = io.compress_onekp_samplelist_hstseqts(onekp_samplelist_genets)
 
-            filename2d = dir2 + "dumpdata2d__k+{}k-{}_{}examples.csv".format(round(km, 4), round(kp, 4), example_n)
-            io.write_dump2d_onekp_time_hst(compressed, filename2d, TIME2)
+        filename2d = dir2 + "dumpdata2d__k+{}k-{}_{}examples.csv".format(round(km, 4), round(kp, 4), example_n)
+        io.write_dump2d_onekp_time_hst(compressed, filename2d, TIME2)
 
 
-def submain(k_plus, k_minus, kn1,ka1,kn2, ka2):
+def submain(k_plus, k_minus, kn1, ka1, kn2, ka2):
     one_variation = np.zeros((example_n, TIME2, 3, HST_N))
     for ex in range(example_n):
         one_variation[ex] = subsubmain(k_plus, k_minus, k_nuc1=kn1, k_ace1=ka1, k_nuc2=kn2, k_ace2=ka2)
@@ -58,6 +57,7 @@ def subsubmain(k_plus, k_minus, k_nuc1, k_ace1, k_nuc2, k_ace2):
     secA = 1
 
     T = 0
+    P =True
 
     histoneList1 = histone.init_genome(percentage=50,
                                        a_bool=A,
@@ -73,18 +73,21 @@ def subsubmain(k_plus, k_minus, k_nuc1, k_ace1, k_nuc2, k_ace2):
                                              a_bool=A,
                                              r_bool=R,
                                              t_bool=T,
+                                             p_bool=P,
                                              ace_prob=k_ace1,
                                              nuc_prob=k_nuc1
                                              )
     # tracker = dictH["vectorize"]
     hstL = dictH["hstL"]
     TList = dictH["TList"]
+    PList = dictH["PList"]
 
     dictH2 = histone.track_epigenetic_process(hst_list=hstL,
                                               time=TIME2,
                                               a_bool=secA,
                                               r_bool=secR,
                                               t_bool=TList[-1],
+                                              p_bool=PList[-1],
                                               ace_prob=k_ace2,
                                               nuc_prob=k_nuc2
                                               )
