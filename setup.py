01bd8c25fa1077a6441018bdebb06971d56bf55e
eifu <etomita18@gmail.com>
2016-08-05 01:55:34 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="epigenetic-switch",
+    version="1.0",
+    description="computational model for histone methylation",
+    author="eifu",
+    author_email="e.tomita225@gmail.com",
+    packages=find_packages(),
+    license="MIT",
+    platforms="any",
+    long_description="a project designed to explore functionality of histones."
+)
