721bc9004cff36c5663348e5d8bc8fc66d7cb1ab
eifu <etomita18@gmail.com>
2016-08-11 16:20:42 -0400

    add scipy function, to fit a data to a exponential function

--- example/packaging_over_knuc_changing.py
+++ example/packaging_over_knuc_changing.py
@@ -3,53 +3,75 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
+from scipy.optimize import curve_fit
 
-dir1 = "example/data/withNUC/"
-example_n = 1000
+dir1 = "example/data/dna_model/"
+example_n = 100
 kp = 0.145
 km = 0.145
 
 kn1 = 1
 
+
+def func(t, a, b, c):
+    return a * np.exp(-b * t) + c
+
+
 def main():
-    ka_list = [0, 0.025, 0.05, 0.075, 0.1]
-    for ka in ka_list:
-        submain(ka)
+    poff_list = [0.0, 0.0025, 0.005, 0.0075]
+    ka_list = [0]
+    for poff in poff_list:
+        for ka in ka_list:
+            submain(poff, ka)
 
 
-def submain(ka):
-
+def submain(poff, ka):
+    # ka = 0
     ka1 = ka
     ka2 = ka
 
     kn2_list = np.arange(0, 0.5, 0.025)
+    # kn2_list=[0.0]
     variation = len(kn2_list)
-    cm_subsection = np.linspace(0.0, 1.0, variation)
+    cm_subsection = np.linspace(0.0, 1.0, 2 * variation)
     colors = [cm.jet(x) for x in cm_subsection]
 
     fig1 = plt.figure()
     ax1 = fig1.add_subplot(111)
     plot_list1 = []
-
+    plot_list2 = []
+    # plot_list_curve = []
 
     for i, kn2 in enumerate(kn2_list):
         # read data
         print("read data...")
-        filename = dir1 + "data{0:d}/kn{1}ka{2}_kn{3}ka{4}/packaging__k+{5}k-{6}_{7:d}examples.csv".format(example_n,
-                                                                                                           kn1,
-                                                                                                           ka1,
-                                                                                                           round(kn2,
-                                                                                                                 4),
-                                                                                                           ka2,
-                                                                                                           kp,
-                                                                                                           km,
-                                                                                                           example_n)
+        filename = dir1 + "data{0:d}/kn{1}ka{2}_kn{3}ka{4}/packaging_p_off{5}_k+{6}k-{7}_{8:d}examples.csv".format(
+            example_n,
+            kn1,
+            ka1,
+            round(kn2,
+                  4),
+            ka2,
+            poff,
+            kp,
+            km,
+            example_n)
         data = np.genfromtxt(filename)
         x = np.arange(504)
 
-        p1 = ax1.plot(x, data, color=colors[i])
+        p1 = ax1.plot(x, data,
+                      color=colors[i],
+                      label="k nuc {}".format(round(kn2,4)))
         plot_list1.append(p1[0])
-
+        try:
+            fit_param, fit_covariance = curve_fit(func, x, data)
+            p1 = ax1.plot(x, func(x, fit_param[0], fit_param[1], fit_param[2]),
+                          color=colors[variation + i],
+                          label=fit_param)
+            plot_list2.append(p1[0])
+            # plot_list_curve.append(fit_paramj
+        except RuntimeError:
+            print("error curve fit failure")
     ax1.set_yscale("log")
 
     ax1.set_xlim(0, 504)
@@ -57,7 +79,7 @@ def submain(ka):
     ax1.set_xticklabels([str(i + 1) + " week" for i in range(3)])
 
     plt.legend(plot_list1[::4], ["k nuc {}".format(round(i, 4)) for i in np.arange(0, 0.5, 0.1)], fontsize=6)
-    title = "packaging_over_knuc_withcolors__log__ka{}.pdf".format(ka)
+    title = "test_dna_model_poff{}_packaging_over_knuc_withcolors__log__ka{}.pdf".format(poff, ka)
     pp = PdfPages(title)
     pp.savefig(fig1)
     pp.close()
@@ -66,8 +88,10 @@ def submain(ka):
     ax1.set_ylim(0, example_n + 1)
     ax1.set_yticks(np.arange(0, example_n + 1, example_n // 10))
     ax1.set_yticklabels([str(i) + "%" for i in range(0, 101, 10)])
-    plt.legend(plot_list1[::4], ["k nuc {}".format(round(i, 4)) for i in np.arange(0, 0.5, 0.1)], fontsize=6)
-    title = "packaging_over_knuc_withcolors__ka{}.pdf".format(ka)
+    l = plt.legend(handles=plot_list1[::4], loc=1, fontsize=6)
+    ax = plt.gca().add_artist(l)
+    plt.legend(handles=plot_list2, loc=2, fontsize=4)
+    title = "test_dna_model_poff{}_packaging_over_knuc_withcolors__ka{}.pdf".format(poff, ka)
     pp = PdfPages(title)
     pp.savefig(fig1)
     pp.close()
