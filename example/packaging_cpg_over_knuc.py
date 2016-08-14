b3202271b03c4c94f33c2ca05734be63cf5fef76
eifu <etomita18@gmail.com>
2016-08-13 20:43:39 -0400

    add more features to the figure

--- example/packaging_cpg_over_knuc.py
+++ example/packaging_cpg_over_knuc.py
@@ -35,25 +35,36 @@ def main():
     colors = [cm.jet(x) for x in cm_subsection]
 
     for i_p_off, p_off in enumerate(p_off_list):
-        for j, kn1 in enumerate(kn1_list):
+        for i_kn1, kn1 in enumerate(kn1_list):
             plot_list = []
-            ax = fig.add_subplot(7,2,i_p_off * 2 + j+1)
+            ax = fig.add_subplot(7,2,i_p_off * 2 + i_kn1+1)
             ax.set_xlim(TIME1, TIME1 + TIME2)
-            for i, kn2 in enumerate(kn2_list):
-                dir2 = dir1 + "kn0ka0_kn0.0ka0/".format(round(kn1, 4),
+            ax.set_xticks([i for i in range(TIME1, TIME1+TIME2, 24*7)])
+            ax.set_xticklabels([str(i/(24*7)) + "week" for i in range(TIME1, TIME1+TIME2, 24 * 7)])
+            ax.set_ylim(-0.5, 100.5)
+            ax.set_yticks(np.arange(0, 101, 20))
+            ax.set_yticklabels(str(i) + "%" for i in range(0, 101, 20))
+            if i_kn1 == 0:
+                ax.set_ylabel("p off: {}".format(round(p_off, 4)), fontsize=4)
+            
+            for i_kn2, kn2 in enumerate(kn2_list): 
+                if i_p_off == 0 and i_kn2 == 0:
+                    ax.set_title("kn1: {}".format(kn1))
+                
+                dir2 = dir1 + "kn{}ka{}_kn{}ka{}/".format(round(kn1, 4),
                                                         round(ka1, 4),
                                                         round(kn2, 4),
                                                         round(ka2, 4))
                 
-                data_pack_filepath = dir2 + "packaging_p_off0.1_k+0.145k-0.145_100examples.csv".format(round(p_off,4),
-                                                                                                       round(kp, 4),
-                                                                                                       round(km, 4),
-                                                                                                       example_n)
+                data_pack_filepath = dir2 + "packaging_p_off{}_k+{}k-{}_{}examples.csv".format(round(p_off,4),
+                                                                                               round(kp, 4),
+                                                                                               round(km, 4),
+                                                                                               example_n)
             
                 
                 data_pack = np.genfromtxt(data_pack_filepath, delimiter=',', dtype=np.int8)
                 t = np.arange(TIME1, TIME1+TIME2)
-                p = ax.plot(t, data_pack, color = colors[i] , label='knuc2: {}'.format(round(kn2, 4)))
+                p = ax.plot(t, data_pack, color = colors[i_kn2] , label='knuc2: {}'.format(round(kn2, 4)))
                 plot_list.append(p[0])
             l = plt.legend(handles=plot_list[::4], loc=1, fontsize=6)
             ax = plt.gca().add_artist(l)
