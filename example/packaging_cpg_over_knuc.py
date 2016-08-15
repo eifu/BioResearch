5ed4e3c1e7bd7cc9fc957fb6191ed31c83c33d93
eifu <etomita18@gmail.com>
2016-08-15 18:46:20 -0400

    fix ticks and labels,

--- example/packaging_cpg_over_knuc.py
+++ example/packaging_cpg_over_knuc.py
@@ -13,6 +13,11 @@ TIME2 = 504
 def main():
 
     p_off_list = [1, 0.5, 0.1, 0.05, 0.01, 0.005, 0.001]
+    for p_off in p_off_list:
+        submain(p_off)
+        print("done {}".format(p_off))
+
+def submain(p_off):
 
     kn1_list = [0, 1]
     kn2_list = np.arange(0, 0.5, 0.025)
@@ -34,77 +39,73 @@ def main():
     cm_subsection = np.linspace(0.0, 1.0, 2 * variation)
     colors = [cm.jet(x) for x in cm_subsection]
 
-    for i_p_off, p_off in enumerate(p_off_list):
-        for i_kn1, kn1 in enumerate(kn1_list):
 
-            ax = fig.add_subplot(14, 2, i_p_off * 4 + i_kn1+1)
-            if i_kn1 == 0:
-                ax.set_ylabel("p off: {}".format(round(p_off, 4)), fontsize=8)
-            ax.set_xlim(0, TIME1 +TIME2)
-            ax.set_xticks(np.arange(0, TIME1+TIME2, 24 * 7))
-            ax.set_xticklabels([str(i//(24*7) + 1) + "week" for i in range(0, TIME1+TIME2, 24*7)])
-            ax.set_ylim(0, 10*example_n)
-            ax.set_yticks(np.arange(0, 1001, 250))
-            ax.set_yticklabels([])
+    for i_kn1, kn1 in enumerate(kn1_list):
+
+        ax = fig.add_subplot(4, 1,  i_kn1 * 2 + 1)
+        if i_kn1 == 0:
+            ax.set_ylabel("p off: {}".format(round(p_off, 4)), fontsize=8)
+        ax.set_xlim(0, TIME1 +TIME2)
+        ax.set_xticks(np.arange(0, TIME1+TIME2, 24 * 7))
+        ax.set_xticklabels([str(i//(24*7) + 1) + "week" for i in range(0, TIME1+TIME2, 24*7)])
+        ax.set_ylim(0, 10*example_n)
+        ax.set_yticks(np.arange(0, 1001, 250))
+        ax.set_yticklabels([])
         
-            plot_list = []
-            bx = fig.add_subplot(14, 4, i_p_off * 8 + i_kn1* 2 + 6)
-            bx.set_xlim(TIME1, TIME1 + TIME2)
-            bx.set_xticks([i for i in range(TIME1, TIME1+TIME2, 24*7)])
-            bx.set_xticklabels([str(i//(24*7) + 1) + "week" for i in range(TIME1, TIME1+TIME2, 24 * 7)])
-            bx.set_ylim(-5, 100.5)
-            bx.set_yticks(np.arange(0, 101, 25))
-            bx.set_yticklabels(str(i) + "%" for i in range(0, 101, 25))
+        plot_list = []
+        bx = fig.add_subplot(4, 1,  i_kn1 * 2 + 2)
+        bx.set_xlim(0, TIME1 + TIME2)
+        bx.set_xticks([i for i in range(0, TIME1+TIME2, 24*7)])
+        bx.set_xticklabels([str(i//(24*7) + 1) + "week" for i in range(0, TIME1+TIME2, 24 * 7)])
+        bx.set_ylim(-5, 100.5)
+        bx.set_yticks(np.arange(0, 101, 25))
+        bx.set_yticklabels(str(i) + "%" for i in range(0, 101, 25))
         
             
-            for i_kn2, kn2 in enumerate(kn2_list):
+        for i_kn2, kn2 in enumerate(kn2_list):
 
-                if i_p_off == 0 and i_kn2 == 0:
-                    ax.set_title("kn1: {}".format(kn1))
+            if i_kn2 == 0:
+                if kn1 == 1:
+                    ax.set_title("kn1: On")
+                else:
+                    ax.set_title("kn1: Off")
                     
-                dir2 = dir1 + "kn{}ka{}_kn{}ka{}/".format(round(kn1, 4),
-                                                          round(ka1, 4),
-                                                          round(kn2, 4),
-                                                          round(ka2, 4))
-                data_cpg_filepath = dir2 + "cpg_sum_p_off{}_k+{}k-{}_{}examples.csv".format(round(p_off, 4),
-                                                                                            round(kp, 4),
-                                                                                            round(km, 4),
-                                                                                            example_n)
-                data_cpg = np.genfromtxt(data_cpg_filepath, delimiter=',')
-                t = np.arange(TIME1 + TIME2)
-                ax.plot(t, data_cpg,
-                        color=colors[i_kn2])
-                print(data_cpg)
+            dir2 = dir1 + "kn{}ka{}_kn{}ka{}/".format(round(kn1, 4),
+                                                      round(ka1, 4),
+                                                      round(kn2, 4),
+                                                      round(ka2, 4))
+            data_cpg_filepath = dir2 + "cpg_sum_p_off{}_k+{}k-{}_{}examples.csv".format(round(p_off, 4),
+                                                                                        round(kp, 4),
+                                                                                        round(km, 4),
+                                                                                        example_n)
+            data_cpg = np.genfromtxt(data_cpg_filepath, delimiter=',')
+            t = np.arange(TIME1 + TIME2)
+            ax.plot(t, data_cpg,
+                    color=colors[i_kn2])
                 
-                data_pack_filepath = dir2 + "packaging_p_off{}_k+{}k-{}_{}examples.csv".format(round(p_off,4),
-                                                                                               round(kp, 4),
-                                                                                               round(km, 4),
-                                                                                               example_n)
+            data_pack_filepath = dir2 + "packaging_p_off{}_k+{}k-{}_{}examples.csv".format(round(p_off,4),
+                                                                                           round(kp, 4),
+                                                                                           round(km, 4),
+                                                                                           example_n)
             
                 
-                data_pack = np.genfromtxt(data_pack_filepath, delimiter=',', dtype=np.int8)
-                t = np.arange(TIME1, TIME1+TIME2)
-                p = bx.plot(t, data_pack,
+            data_pack = np.genfromtxt(data_pack_filepath, delimiter=',', dtype=np.int8)
+            t = np.arange(TIME1, TIME1+TIME2)
+            p = bx.plot(t, data_pack,
                             '-',
                             color = colors[i_kn2],
                             label='knuc2: {}'.format(round(kn2, 4)),
                 )
-                plot_list.append(p[0])
+            plot_list.append(p[0])
 
 
-            l = plt.legend(handles=plot_list[::4], loc=1, fontsize=6)
-            bx = plt.gca().add_artist(l)
-             
-    plt.show()
+        l = plt.legend(handles=plot_list[::4], loc=1, fontsize=6)
+        bx = plt.gca().add_artist(l)
 
-
-    title = "test_dna_model_over_poff_packaging_over_knuc_withcolors.pdf"
+    title = "packaging_over_poff{}_over_knuc_withcolors.pdf".format(p_off)
     pp = PdfPages(title)
     pp.savefig(fig)
     pp.close()
 
 if __name__ == "__main__":
     main()
-    
-        
-        
