6e7f36cd35b9cbf5b638c1f2a31745ddf22be340
eifu <etomita18@gmail.com>
2016-10-31 11:34:02 -0400

    modify and make week3 histogram program more general purpose

--- example/week_by_week_histogram.py
+++ example/week_by_week_histogram.py
@@ -8,16 +8,15 @@ dir1 = "example/data/withNUC/data{}/".format(example_n)
 hst_n = 81
 kp = 0.145
 km = 0.145
-kn2_list = np.arange(0, 0.05, 0.0025)  # length 20
+kn2_list = np.arange(0.03, 0.05, 0.0025)  # length 20
 
 
 def main():
-#     ka_list = [0, 0.025, 0.05, 0.075, 0.1]  # length 5
-#     for ka in ka_list:
-#         submain(ka)
-#
-#
-# def submain(ka):
+    for week in range(0, 7):
+        submain(week)
+
+
+def submain(week):
 
     ka = 0
 
@@ -70,14 +69,14 @@ def main():
         data = np.genfromtxt(filename, delimiter=',', dtype=np.int8)
 
         container = np.zeros(12)  # from 0 to 11. total 12 paterns
-        for _, row in enumerate(data):
+        for _, row in enumerate(data[24 * 7 * week: 24 * 7 * (week+1)]):
             container[sum((row + row * row) / 2)] += 1
         ax.set_ylim(0, max(container))
         ax.bar([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], container)
 
         print(i_kn2 / 20)
 
-    title = "week3_histogram__ka{}__k+{}k-{}_N{}.pdf".format(round(ka, 4),
+    title = "week{}_histogram__ka{}__k+{}k-{}_N{}.pdf".format(week,round(ka, 4),
                                                              round(kp, 4),
                                                              round(km, 4),
                                                              example_n)
